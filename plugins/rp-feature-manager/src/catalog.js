9cf45be90a1ae6bced860f8ed7eafed602d865d2
lutrodev <320952244+lutrodev@users.noreply.github.com>
2026-08-26 16:29:44 +0800

    chore: release v0.1.3

--- plugins/rp-feature-manager/src/catalog.js
+++ plugins/rp-feature-manager/src/catalog.js
@@ -1,6 +1,6 @@
 /** Product-facing Roleplay feature catalog and release compatibility contract. */
 
-export const ROLEPLAY_SUITE_VERSION = '0.1.2'
+export const ROLEPLAY_SUITE_VERSION = '0.1.3'
 export const SUPPORTED_DSH_RANGE = '^0.1.1-rc.2'
 export const SETTINGS_NAMESPACE = 'roleplay-features'
 
